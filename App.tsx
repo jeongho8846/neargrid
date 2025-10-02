162d18bdbff35efc0b498178627b25d35a8a6dab
jeonjeonhho <calseolralri@naver.com>
2025-10-02 16:32:37 +0900

    app.tsx에 재스쳐핸들러와 세이프에어리어프로바이더 추가

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigators/RootNavigator';
 import './src/i18n'; // ✅ i18n 초기화
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <RootNavigator />
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={styles.container}>
+      <SafeAreaProvider>
+        <RootNavigator />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
--- src/common/components/Input/index.tsx
+++ src/common/components/Input/index.tsx
@@ -2,16 +2,19 @@ import React, { forwardRef } from 'react';
 import { TextInput, TextInputProps, StyleSheet, View } from 'react-native';
 import { COLORS, SPACING, FONT } from '../../styles';
 import AppText from '../AppText';
+import { useTranslation } from 'react-i18next';
 
 type Props = TextInputProps & {
   labelKey?: string;
   placeholderKey?: string;
   error?: string;
-  nextInputRef?: React.RefObject<TextInput | null>; // ✅ null 허용
+  nextInputRef?: React.RefObject<TextInput | null>;
 };
 
 const AppInput = forwardRef<TextInput, Props>(
   ({ labelKey, placeholderKey, error, style, nextInputRef, ...rest }, ref) => {
+    const { t } = useTranslation(); // ✅ i18n hook 사용
+
     return (
       <View style={{ marginBottom: SPACING.md }}>
         {labelKey && (
@@ -19,18 +22,18 @@ const AppInput = forwardRef<TextInput, Props>(
         )}
 
         <TextInput
-          ref={ref} // ✅ forwardRef로 연결
+          ref={ref}
           style={[styles.input, error && styles.errorBorder, style]}
-          placeholder={placeholderKey}
+          placeholder={placeholderKey ? t(placeholderKey) : undefined} // ✅ 번역 처리
           placeholderTextColor={COLORS.border}
           blurOnSubmit={!!nextInputRef ? false : rest.blurOnSubmit}
           returnKeyType={nextInputRef ? 'next' : rest.returnKeyType}
           onSubmitEditing={e => {
             if (nextInputRef?.current) {
-              nextInputRef.current.focus(); // ✅ 다음 인풋으로 이동
+              nextInputRef.current.focus();
             }
             if (rest.onSubmitEditing) {
-              rest.onSubmitEditing(e); // 기존 핸들러도 실행
+              rest.onSubmitEditing(e);
             }
           }}
           {...rest}
